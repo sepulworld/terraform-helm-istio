f094b3fd996541dae4d956c507559b7636abc3b5
Zane Williamson <zwilliamson@flexport.com>
2022-05-06 14:59:13 -0700

    skip val

--- tests/providers.tf
+++ tests/providers.tf
@@ -7,4 +7,8 @@ provider "helm" {}
 provider "aws" {
   skip_credentials_validation = true
   skip_requesting_account_id  = true
+  skip_get_ec2_platforms = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
+  skip_region_validation = true
 }
