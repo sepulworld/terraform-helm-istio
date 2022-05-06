a5524e0a46f7b97c8b6a63e6aef1a5f640335fff
Zane Williamson <zwilliamson@flexport.com>
2022-05-06 15:03:16 -0700

    removing dup

--- tests/providers.tf
+++ tests/providers.tf
@@ -8,8 +8,7 @@ provider "aws" {
   // Skipping because we don't test aws infra in test pipeline
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  skip_get_ec2_platforms = true
-  skip_metadata_api_check = true
-  skip_requesting_account_id = true
-  skip_region_validation = true
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
 }
